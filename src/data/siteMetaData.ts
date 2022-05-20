8c3fa25e4bae3ec858c09f26238bb26aad9b8674
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-05-20 17:06:21 +0900

    seo

--- src/data/siteMetaData.ts
+++ src/data/siteMetaData.ts
@@ -4,9 +4,9 @@ export const siteMetadata = {
   description: "ポレポーレは、座間市で活動する女性合唱団です。団員募集中✨",
   language: "jp-ja",
   siteUrl: "https://www.polepole.netlify.app",
-  siteLogo: "/static/images/logo.jpg",
-  image: "/static/images/logo.jpg",
+  siteLogo: "/static/images/logo.png",
+  image: "/static/images/logo.png",
   email: "yoko.iwasaki8@gmail.com",
   locale: "ja-JP",
-  altForImages: "製品イメージ図",
+  altForImages: "イメージ図",
 };
