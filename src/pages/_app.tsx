3dd37deb5bbf51059f36f3f0ef4e8250e64a5335
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-05-30 09:11:38 +0900

    ✨ add click-to-component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "/src/style/index.css";
 
+import { ClickToComponent } from "click-to-react-component";
 import type { AppProps } from "next/app";
 // import { GoogleAnalytics } from "src/component/GoogleAnalytics";
 import { usePageView } from "src/lib/usePageView";
@@ -8,6 +9,7 @@ const App = (props: AppProps) => {
   usePageView();
   return (
     <>
+      <ClickToComponent />
       {/* <GoogleAnalytics /> */}
       <props.Component {...props.pageProps} />
     </>
